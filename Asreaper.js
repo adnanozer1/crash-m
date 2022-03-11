92ddd43767626460f1f792a93a2302f0e30d456e
Glitch (floxy-m) <none>
2022-03-11 16:51:23 +0000

    ☂️🐘 Updated with Glitch

---
+++ Asreaper.js
@@ -0,0 +1,63 @@
+const discord = require("discord.js")
+
+const client = new discord.Client({ disableEveryone: true, disabledEvents: ["TYPING_START"] });
+const { readdirSync } = require("fs");
+const { join } = require("path");
+const { TOKEN, PREFIX } = require("./config.json")
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Nays Uptime & FastUptime'));
+
+app.listen(port, () =>
+    console.log(`Port Açıldı! ${port}`)
+);
+
+client.on("ready", () => {
+  console.log('Ready TO play some soft songs')
+  client.user.setActivity("!yardım")
+})
+
+client.on("warn", info => console.log(info));
+
+client.on("error", console.error)
+
+client.commands = new discord.Collection()
+client.prefix = PREFIX
+client.queue = new Map();
+
+
+const cmdFiles = readdirSync(join(__dirname, "commands")).filter(file => file.endsWith(".js"))
+for (const file of cmdFiles) {
+  const command = require(join(__dirname, "commands", file))
+  client.commands.set(command.name, command)
+} 
+
+
+client.on("message", message => {
+   if (message.author.bot) return;
+  if (!message.guild) return;
+  
+  if(message.content.startsWith(PREFIX)) {
+    
+    const args = message.content.slice(PREFIX.length).trim().split(/ +/)
+    const command = args.shift().toLowerCase();
+    
+    if(!client.commands.has(command)) {
+      return;
+    } 
+    
+  try  { 
+      client.commands.get(command).execute(client, message, args)
+    } catch (err) { 
+      console.log(err)
+      message.reply("I am getting error on using this command")
+    }
+    
+  }
+  
+  
+});
+
+client.login(process.env.token)
